8e161cc1acd6af2870f1eac281537d5cc10dbd0e
Roumya Das <roumyadas@Roumyas-MacBook-Air.local>
2025-06-13 01:04:19 +0530

    special stats tab 17

--- script.js
+++ script.js
@@ -351,7 +351,7 @@ const tabSpecialstatsnames = [
   "Pitch-wise Bowler", "Bowl-type wise Batter", "Fastest-100", "Fastest-50",
   "Fastest-5W-haul", "Fastest_3W-haul", "Pitch-Inning-wise Win%", 
   "Form_inning_1 Batter", "Form_inning_2 Batter", "Form_inning_1 Bowler",
-  "Form_inning_2 Bowler", "Venue stats", "stats17", "stats18",
+  "Form_inning_2 Bowler", "Venue stats", "Pos.n-wise Batter (min. 100 balls)", "stats18",
   "stats19", "stats20", "stats21", "stats22", "stats23", "stats24",
   "stats25" // fill with whatever name you want
 ];
