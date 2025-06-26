a35508688b966194d40fbe03589914366f7f3bd7
Roumya Das <roumyadas@Roumyas-MacBook-Air.local>
2025-06-26 22:25:27 +0530

    RR cap

--- script.js
+++ script.js
@@ -1171,7 +1171,7 @@ document.addEventListener("DOMContentLoaded", () => {
     '250625_recap.txt','160625_SRH_MI_recap.txt','240625_Liam_dim.txt','250625_PBKS_GT_thriller.txt',
     '240625_Allen_fire.txt','260625_GT_debuts.txt','230625_GT_RCB_thriller.txt','220625_DC_injury.txt','170625_MI_caps.txt',
     '190625_RR_ownership.txt','240625_MI_injury.txt','160625_GT_caps.txt','260625_Big_Show_Maxi.txt','260625_Conway_vanish.txt',
-    '260625_LSG_RCB.txt','260625_CSK_SRH.txt']
+    '260625_LSG_RCB.txt','260625_CSK_SRH.txt','260625_RR_caps.txt']
     ;
 
   const newsData = []; // will store all stories
