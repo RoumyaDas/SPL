220a8a6c43a782901fc232987f0f7723940e9577
Roumya Das <roumyadas@Roumyas-MacBook-Air.local>
2025-06-26 12:52:51 +0530

    news

--- script.js
+++ script.js
@@ -1166,7 +1166,8 @@ document.addEventListener("DOMContentLoaded", () => {
     "250625_PBKS_GT_thriller.txt",
     "260625_LSG_Stokes_missing.txt",
     "230625_GT_RCB_thriller.txt",
-    "250625_recap.txt"
+    "250625_recap.txt",
+    "160625_SRH_MI_recap.txt"
   ];
 
   // LIFO - latest first
