fc3297937a30038687b74e8178dc44f21a8dfe3e
Roumya Das <roumyadas@Roumyas-MacBook-Air.local>
2025-06-26 21:46:40 +0530

    match 26

--- script.js
+++ script.js
@@ -1170,7 +1170,8 @@ document.addEventListener("DOMContentLoaded", () => {
   const newsFiles = ['260625_LSG_Stokes_missing.txt','250625_KKR_trumps_RR.txt','250625_KKR_duo.txt','240625_CSK_MI.txt',
     '250625_recap.txt','160625_SRH_MI_recap.txt','240625_Liam_dim.txt','250625_PBKS_GT_thriller.txt',
     '240625_Allen_fire.txt','260625_GT_debuts.txt','230625_GT_RCB_thriller.txt','220625_DC_injury.txt','170625_MI_caps.txt',
-    '190625_RR_ownership.txt','240625_MI_injury.txt','160625_GT_caps.txt','260625_Big_Show_Maxi.txt','260625_Conway_vanish.txt']
+    '190625_RR_ownership.txt','240625_MI_injury.txt','160625_GT_caps.txt','260625_Big_Show_Maxi.txt','260625_Conway_vanish.txt',
+    '260625_LSG_RCB.txt','260625_CSK_SRH.txt']
     ;
 
   const newsData = []; // will store all stories
